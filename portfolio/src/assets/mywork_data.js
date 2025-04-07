725a5761c4fa46a5d0b53783c4497f6006fbf073
mpggekweerasinghe <gayaniweerasinghe2001@gmail.com>
2025-04-07 15:15:16 +0530

    update mywork

--- portfolio/src/assets/mywork_data.js
+++ portfolio/src/assets/mywork_data.js
@@ -1,7 +1,7 @@
-import logo1 from '../assets/logo.png'
-import logo2 from '../assets/logo.png'
-import logo3 from '../assets/logo.png'
-import logo4 from '../assets/logo.png'
+import logo1 from '../assets/logo1.png'
+import logo2 from '../assets/logo2.png'
+import logo3 from '../assets/logo3.png'
+import logo4 from '../assets/logo4.png'
 
 const mywork_data = [
     {
